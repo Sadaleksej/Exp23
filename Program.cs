3b13d6de1bfaf3318aeddd58f660f2b9bf4ba097
Sadaleksej <135204154+Sadaleksej@users.noreply.github.com>
2023-06-16 19:45:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Введите N: ");
+int i = 1;
+int N = int.Parse(Console.ReadLine());
+if (N<1) {
+Console.WriteLine("Введено некорректное значение, попытайтесь еще");
+return;
+}
+for(i=1; i<=N; i++) Console.WriteLine($"Куб {i} равен {Math.Pow(i,3)}");
